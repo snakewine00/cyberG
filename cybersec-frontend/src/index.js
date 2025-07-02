035ed155abb6ca1842e5e8d85ee853a253b6ac48
snakewine00 <varunprasathk22@gmail.com>
2025-07-02 23:55:52 +0530

    Update project files and dependencies

--- cybersec-frontend/src/index.js
+++ cybersec-frontend/src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'; // Ensure this is imported to apply global styles
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
